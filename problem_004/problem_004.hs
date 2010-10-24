9674c243ca19ef889729a8fbf4215edb885fdf84
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2010-10-25 01:27:01 +0900

    added haskll version solution for problem 004

---
+++ problem_004/problem_004.hs
@@ -0,0 +1,24 @@
+module Main (main) where
+
+-- Is a value palindromic?
+isPalindromic n = reverse (show n) == show n
+
+-- generate palindrome numbers and return greater one
+palindrome :: Int -> Int -> Int -> Int -> Int
+palindrome min max n1 n2
+  | n1 > n2 = f min (n2 + 1) -- have calced at reverse pair: 123*122 == 122*123
+  | n2 >= max = 0            -- stop if reached at max
+  | n1 >= max = getResult (f min (n2 + 1)) -- next n2
+  | otherwise = getResult (f (n1 + 1) n2)  -- next n1
+    where
+      n = n1 * n2
+      f = palindrome min max
+      getResult rec = if isPalindromic n && n > rec then n else rec
+
+-- get max palindrome and print it
+main =
+  putStrLn (show n)
+    where
+      min = 100
+      max = 1000
+      n = palindrome min max min min
