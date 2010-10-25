8b4e16ae775696f3ae17bc5f95cc17aed22a8228
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2010-10-26 02:06:22 +0900

    made problem 001 dir and used print

---
+++ problem_001/problem_001.scm
@@ -0,0 +1,23 @@
+;; project euler: problem 1
+;; scheme version
+;; Keita Yamaguchi
+
+(use srfi-1)
+
+(define (mul i n)
+  (if (< n 1000) (cons n (mul i (+ n i))) '()))
+(define mul3 (mul 3 0))
+(define mul5 (mul 5 0))
+(define multiples (lset-union eq? mul3 mul5))
+
+(print "multiples of 3 below 1000:")
+(print mul3)
+
+(print "multiples of 5 below 1000:")
+(print mul5)
+
+(print "multiples of 3 or 5 below 1000:")
+(print multiples)
+
+(print "Answer: ")
+(print (fold + 0 multiples))
