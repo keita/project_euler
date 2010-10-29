4fe817b6ddd124dc8c7efa1b434860a25d8aac60
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2010-10-30 08:54:54 +0900

    added haskell version solution for problem 001

---
+++ problem_001/problem_001.hs
@@ -0,0 +1,12 @@
+-- project euler: problem 1
+-- Keita Yamaguchi, 2010
+-- Haskell version
+
+module Main(main) where
+import List
+
+mul n = [x | x <- [1..1000], (x `mod` n) == 0]
+
+main = print (show res)
+  where
+  res = sum ((mul 3) `union` (mul 5))
